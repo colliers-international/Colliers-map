b86693b63f612ccc3c9dbd1c6da5c5ef445f5cf9
Jacob <jacob.nelson@colliers.com>
2024-12-23 09:15:41 -0700

    removed exclude publish mapbox-gl

--- tsup.config.ts
+++ tsup.config.ts
@@ -4,7 +4,6 @@ export default defineConfig({
     entry: ['src/index.ts'], 
     format: ['cjs', 'esm'],  
     dts: true,               
-    external: ['mapbox-gl'], // Prevent bundling mapbox-gl
     splitting: false,        
     clean: true,             
 });
